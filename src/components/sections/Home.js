1ee176e0c41bae4d7bce76254de8d2b4fffd3164
mikemorter <mikemorter@gmail.com>
2022-09-15 09:49:04 -0700

    Add Home Page

---
+++ src/components/sections/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../elements/Button";
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home Page</p>
+      <Button value="Submit" color="primary" size="lg" wideMobile>
+        Submit
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
